1673ce10dde6663e210294bee66916dc17a4f2bf
Paweł Stech <pawelstech95@gmail.com>
2021-04-21 21:54:46 +0200

    react-hook-form first attempt

--- src/containers/AddRecipePage.jsx
+++ src/containers/AddRecipePage.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
- 
+import { useForm } from 'react-hook-form';
 const AddRecipe = () => {
-    return ( <div>przepis</div>  );
-}
- 
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (values) => console.log(values); // w dokumentacji - data
+  console.log(errors, 'errors')
+  return (
+    console.log('render'),
+    (
+      <>
+        <h2>Create recipe</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            Title: <br/>
+            <input
+              name="title"
+              {...register}
+              type="text"
+              // value={''}
+              placeholder=" e.g. pizza"
+            />
+          </label>
+          <label>
+            Ingredients: <br/>
+            <select {...register('Ingredients')} multiple>
+              <option value="chease">chease</option>
+              <option value="milk">milk</option>
+              <option value="other">other</option>
+            </select>
+          </label>
+          <label>
+            Description <br/>
+            <textarea type="text" placeholder="Description..." />
+          </label>
+          <button className="btn btn__add" type="submit">
+            Add
+          </button>
+          <button className="btn btn__remove">Clear</button>
+        </form>
+      </>
+    )
+  );
+};
+
 export default AddRecipe;
+
+// <form>
+//         <label>
+//           Title
+//           <input type="text" value={''} placeholder=" e.g. pizza" />
+//         </label>
+//         <br />
+//         <br /> <br />
+//         <>
+//           <input type="checkbox" />
+//           <label> Cheasse</label>
+//           <br />
+//           <input type="checkbox" />
+//           <label> Tomatoes</label>
+//           <br />
+//           <input type="checkbox" />
+//           <label> Pineapple</label>
+//         </>
+//         <br />
+//         <br />
+//         <br />
+//         <label>
+//           Description
+//           <textarea type="text" value={''} placeholder="Description..." />
+//         </label>
+//         <br/>
+//         <button className='btn'>Add</button>
+//         <button className='btn'>Clear</button>
+//       </form>
