f8b75713086fa801906172a533c4f7c22e4fc563
Paweł Stech <pawelstech95@gmail.com>
2021-04-22 11:44:08 +0200

    added default state to useForm

--- src/containers/AddRecipePage.jsx
+++ src/containers/AddRecipePage.jsx
@@ -1,53 +1,62 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 const AddRecipe = () => {
+  // useForm
+  const defaultState = { title: '', ingredients: [], description: '' };
   const {
     register,
     handleSubmit,
     // watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (values) => console.log(values); // w dokumentacji - data
-  console.log(errors, 'errors');
+    // formState: { errors },
+  } = useForm(defaultState);
+  const onSubmit = (values) => console.log(values);
+
+  //   console.log(errors, 'errors');
+
+  // Clean button
+  const handleClear = (e) => {
+    e.preventDefault();
+    const clear = HTMLFormElement.reset(defaultState);
+    clear()
+  };
   return (
-    console.log('render'),
-    (
-      <>
-        <h2>Create recipe</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label>
-            Title: <br />
-            <input
-              name="title"
-              {...register('Title')}
-              type="text"
-              // value={''}
-              placeholder=" e.g. pizza"
-            />
-          </label>
-          <label>
-            Ingredients: <br />
-            <select {...register('Ingredients')} multiple>
-              <option value="chease">chease</option>
-              <option value="milk">milk</option>
-              <option value="other">other</option>
-            </select>
-          </label>
-          <label>
-            Description <br />
-            <textarea
-              {...register('Description')}
-              type="text"
-              placeholder="Description..."
-            />
-          </label>
-          <button className="btn btn__add" type="submit">
-            Add
-          </button>
-          <button className="btn btn__remove">Clear</button>
-        </form>
-      </>
-    )
+    <>
+      <h2>Create recipe</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Title: <br />
+          <input
+            name="title"
+            {...register('title')}
+            type="text"
+            // value={''}
+            placeholder=" e.g. pizza"
+          />
+        </label>
+        <label>
+          Ingredients: <br />
+          <select {...register('ingredients')} multiple>
+            <option>chease</option>
+            <option>milk</option>
+            <option>other</option>
+          </select>
+        </label>
+        <label>
+          Description <br />
+          <textarea
+            {...register('description')}
+            type="text"
+            placeholder="Description..."
+          />
+        </label>
+        <button className="btn btn__add" type="submit">
+          Add
+        </button>
+        <button className="btn btn__remove" onClick={handleClear}>
+          Clear
+        </button>
+      </form>
+    </>
   );
 };
 
