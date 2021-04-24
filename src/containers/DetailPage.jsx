5e9164c67d7bf50e9884a47bd956e241cc387902
Paweł Stech <pawelstech95@gmail.com>
2021-04-24 10:35:52 +0200

    added form to the EditRecipePage

--- src/containers/DetailPage.jsx
+++ src/containers/DetailPage.jsx
@@ -6,8 +6,7 @@ import { data } from '../utils';
 const DetailPage = ({ match }) => {
   const id = match.params.id * 1; // ??
 
-  //   const [state, setState] = useState(data);
-  const displayDetail = data.map((recipe) =>
+  const displayRecipe = data.map((recipe) =>
     recipe.id === id ? (
       <li key={recipe.id}>
         <h2>{recipe.title}</h2>
@@ -18,22 +17,14 @@ const DetailPage = ({ match }) => {
     ) : null
   );
 
-//   console.log(typeof(displayDetail))
+//   console.log(data);
+//   console.log(id, 'params');
+//   console.log(displayRecipe);
 
-  // const newData = setNewItem(item.filter((recipe) => recipe.id !== id));
-  //   const newData = data.map(item => <li>{item.title}</li>);
-  //   console.log(data); // [{},{}]
-  console.log(id, 'params');
-  console.log(displayDetail);
-
-  // const list = products.map((product) => (
-  //     <li key={product}>
-  //       <Link to={`/product/${product}`}>{product}</Link>
-  //     </li>
   return (
     <div className="recipe__detailPage">
       <h2>Recipe</h2>
-      <ul>{displayDetail}</ul>
+      <ul>{displayRecipe}</ul>
     </div>
   );
 };
--- src/containers/EditRecipePage.jsx
+++ src/containers/EditRecipePage.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import Recipe from '../components/Recipe'
-const EditRecipePage = () => {
-    return ( <> <Recipe /></>);
-}
- 
-export default EditRecipePage;
+import Form from '../components/Form';
+
+import { data } from '../utils';
+
+const DetailPage = ({ match }) => {
+  const id = match.params.id * 1; // ??
+
+  //   const [state, setState] = useState(data);
+  const displayRecipe = data.map(
+    (recipe) => recipe.id === id && <h2 key={recipe.id}>{recipe.title}</h2>
+  );
+
+  //   console.log(data)
+  //   console.log(id, 'params');
+  //   console.log(displayDetail);
+
+  return (
+    <div className="recipe__detailPage">
+      <h2>Recipe</h2>
+      {displayRecipe}
+      <Form 
+      />
+    </div>
+  );
+};
+
+export default DetailPage;
